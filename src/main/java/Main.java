df6d4a0012e9a589aafcf451e3b899095bdd0af0
shuvornb <shuvornb@gmail.com>
2019-12-03 09:53:26 -0500

    menu option 3 and 4 implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -71,10 +71,77 @@ public class Main {
                 }
             }
             else if(input == 3) {
+                MinHash.populateHashFunctions();
+                System.out.println("Please enter the location path of the directory of your html file");
+                scanner.nextLine();
+                String directoryPath = scanner.nextLine();
+
+                Email email = DataPreprocessor.importHTMLFiles(directoryPath).get(0);
+                String sql = "SELECT MAX(id) FROM email";
+
+                DatabaseConnector dc = new DatabaseConnector();
+                Connection connection = dc.connect();
+
+                try {
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    ResultSet rs = statement.executeQuery();
+                    int maxId = 0;
+                    if(rs.next()) maxId = rs.getInt(1);
+                    System.out.println("Max Id found: " + maxId);
+                    email.id = maxId + 1;
+                    ArrayList<Email> emails = new ArrayList<Email>();
+                    emails.add(email);
+                    DataPreprocessor.storeProcessedEmails(emails);
+
+                    ClusteringService.clusterSingleEmail(email);
+
+                    dc.close(connection);
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
 
             }
             else if(input == 4) {
 
+                System.out.println("Please enter the directory path where you want to write");
+                scanner.nextLine();
+                String directoryPath = scanner.nextLine();
+
+                DatabaseConnector dc = new DatabaseConnector();
+                Connection connection = dc.connect();
+                ArrayList<Cluster> clusters = ClusteringService.readAllExistingCluster(connection);
+
+                int clusterCounter = 0;
+                for(Cluster cluster: clusters) {
+                    clusterCounter++;
+                    ArrayList<String> emailIds = new ArrayList<String>(cluster.emailSet);
+
+                    for(String id: emailIds) {
+                        String sql = "SELECT * FROM email WHERE id = ?";
+                        try {
+                            PreparedStatement statement = connection.prepareStatement(sql);
+                            statement.setInt(1, Integer.parseInt(id));
+                            ResultSet rs = statement.executeQuery();
+                            Email email = new Email();
+                            if(rs.next()) {
+                                email.id = rs.getInt(1);
+                                email.date = rs.getString(2);
+                                email.from = rs.getString(3);
+                                email.to = rs.getString(4);
+                                email.subject = rs.getString(5);
+                                email.content = rs.getString(6);
+                            }
+                            String filePath = directoryPath + "Cluster" + clusterCounter + "\\email" + email.id + ".html";
+                            System.out.println("Writing File: " + filePath);
+                            ClusteringService.writeUsingOutputStream(email.content, filePath);
+
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                dc.close(connection);
             }
             else {
                 System.out.println("Please press according to the menu.");
