002e7b0c0b05b8bfa6a5b3ef70d7ab72bf6fbd94
shuvornb <shuvornb@gmail.com>
2019-12-02 20:58:18 -0500

    database property name updated

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -3,14 +3,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
-    private final String url = "jdbc:postgresql://localhost:5432/crusher_server";
-    private final String user = "postgres";
-    private final String password = "1234567t";
+    private final String DATABASE_NAME = "crusher_server";
+    private final String URL = "jdbc:postgresql://localhost:5432/" + DATABASE_NAME;
+    private final String USER = "postgres";
+    private final String PASSWORD = "1234567t";
 
     public Connection connect() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected to the PostgreSQL server successfully.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
